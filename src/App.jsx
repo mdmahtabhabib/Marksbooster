b23d6aa198e32b8684c6d0f09012616eed740229
mdmahtabhabib <104494125+mdmahtabhabib@users.noreply.github.com>
2026-05-16 14:10:56 +0530

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,22 @@ function App() {
       </div>
       <div className="flex items-center gap-3">
         
-        <button href="#" className="bg-blue-700 text-white text-sm px-5 py-2 rounded-xl transition-colors hover:bg-blue-800">
+        <a href="#" className="bg-blue-700 text-white text-sm px-5 py-2 rounded-xl transition-colors hover:bg-blue-800">
           Login/Register
-        </button>
+        </a>
       </div>
     </nav>
+    <section className=" bg-blue-100 py-16 md:py-32 px-5 text-center  ">
+      <h1 className="text-xl md:text-3xl font-bold text-blue-900">The smartest way for <br /> 
+      class 8-10 students to <br />
+       <span className="text-blue-500"> study & score higher</span></h1>
+      <p className="text-gray-600 text-sm md:text-lg max-w-2xl mx-auto mt-3 mb-8 leading-relaxed">Flashcard , PYQ , doubt solver, mind maps , syllabus tracker -everything you needs in one simple platform. CBSE & State Board . just at 499/year</p>
+      <div>
+        <a href="#" className="bg-blue-600 text-white px-3 py-2 rounded-md">
+          choose your class
+        </a>
+      </div>
+    </section>
     </div>
   )
 }
