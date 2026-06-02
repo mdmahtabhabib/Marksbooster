0cd93b906bd786134b436197e18148e5aea73523
mdmahtabhabib <104494125+mdmahtabhabib@users.noreply.github.com>
2026-06-02 19:36:41 +0530

    added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import {  BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Neetlist from "./Pages/Neetlist";
+import ScrollToTop from "./Components/ScrollToTop";
 
 
 function App() {
   return (
   <BrowserRouter>
+   < ScrollToTop />
     <Routes>
      <Route path="/" element={<Home />} />
      <Route path="/Neetlist" element={< Neetlist />} />
