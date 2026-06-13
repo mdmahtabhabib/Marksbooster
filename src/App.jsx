6d28a4da640cc21faa74b83d95e47a8293c4e1f1
mdmahtabhabib <104494125+mdmahtabhabib@users.noreply.github.com>
2026-06-14 01:59:38 +0530

    fix App.jsx PlantGD.JS import

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,6 @@ import ComingSoon from "./Pages/ComingSoon";
 import Cell from "./Neet/Cell";
 import Cards from "./comp/Card";
 import DynamicChapter from "./Pages/DynamicChapter";
-import BiotechnologyApplications from "./Neet/BiotechnologyApplications";
-import LivingWorld from "./Neet/LivingWorld";
-import BiologicalClassification from "./Neet/BiologicalClassification";
-import PlantKingdom from "./Neet/PlantKingdom";
-import AnimalKingdom from "./Neet/AnimalKingdom";
-import PlantMorphology from "./Neet/PlantMorphology";
-import PlantAnatomy from "./Neet/PlantAnatomy";
-import CellUnitOfLife from "./Neet/CellUnitOfLife";
-import Biomolecules from "./Neet/Biomolecules";
-import Celldivision from "./Neet/Celldivision";
-import Photosynthesis from "./Neet/Photosynthesis";
-import Respiration from "./Neet/Respiration";
-import PlantGD from "./Neet/PlantGD.JS";
-import Breathingandexchange from "./Neet/Breathingandexchange";
-import Bodyfluids from "./Neet/Bodyfluids";
-import Excretoryproduct from "./Neet/Excretoryproduct";
-import Locomotionandmovement from "./Neet/Locomotionandmovement";
-import Neuralcontrol from "./Neet/Neuralcontrol";
-import Chemicalcoordination from "./Neet/Chemicalcoordination";
 
 
 function App() {
