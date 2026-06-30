faf9a7e48a3c315a1324a2ccbbf68441f5af0981
mdmahtabhabib <104494125+mdmahtabhabib@users.noreply.github.com>
2026-07-01 03:43:25 +0530

    making sign up page added key and url for supabse in env

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import {  BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { supabase } from "./lib/supabase" ;
 import Home from "./Components/Home";
 import Neetlist from "./Pages/Neetlist";
 import ScrollToTop from "./Components/ScrollToTop";
@@ -15,13 +17,27 @@ import Subjectlist from "./app/Subjectlist";
 import SubjectlistPage from "./app/SubjectlistPage";
 import Chapterlist from "./app/Chapterlist";
 import ChapterlistPage from "./app/ChapterlistPage";
+import Signup from "./auth/Signup";
 
 
 
 
 
 function App() {
+
+  useEffect(() => {
+     async function checkSession() {
+      const { data , error } = await supabase.auth.getSession();
+      console.log(data);
+      console.log(error);
+    } 
+    checkSession();
+    
+      
+     } 
+      , [] );
   return (
+    <div>
   <BrowserRouter>
    < ScrollToTop />
     <Routes>
@@ -33,10 +49,14 @@ function App() {
     
      <Route path="/home/:classId/:tool" element={<SubjectlistPage /> } />
       <Route path="/home/:classId/:tool/:subject" element={<ChapterlistPage />} />
+      <Route path="/signup" element={<Signup />} />
      
      
     </Routes> 
     </BrowserRouter>
+
+    
+    </div>
   )
 }
 
