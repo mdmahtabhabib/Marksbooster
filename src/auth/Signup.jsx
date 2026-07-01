30249b71a41a81f12c62fb7183441087ad7958ae
mdmahtabhabib <104494125+mdmahtabhabib@users.noreply.github.com>
2026-07-02 01:17:37 +0530

    added sign up page now it is working but very basic level

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,10 +1,34 @@
 import { useState } from "react";
+import { supabase } from "../lib/supabase";
+
+
+
 
 
 export default function Signup() {
+    const [email, setEmail] = useState("") ;
+const [password, setPassword] = useState("");
+ async function handleSubmit(e){
+    e.preventDefault();
+    const {data , error } = await supabase.auth.signUp({
+        email , 
+        password,
+    });
+    if (error) {
+        console.log("ERROR" , error );
+    } else {
+        console.log("SUCCESS:" , data)
+    }
+};
     return (
+        
         <div>
-            <h1>hello</h1>
+           <form onSubmit={handleSubmit} >
+            <input className="border rounded-lg px-3 py-2" value={email} type="email" onChange={(e) =>{setEmail(e.target.value)}} />
+            <input className="border rounded-lg px-3 py-2" value={password} type="password" onChange={(e) =>{setPassword(e.target.value)}} />
+            <button type="submit" >Sign Up</button>
+            
+           </form>
         </div>
     );
 };
